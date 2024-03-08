4b67b592e6579f3eab1a39f2f2fb4128dcf46d21
Diana <dianaajambo@gmail.com>
2024-03-08 14:55:54 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/layout/Header"
+import Home from "./components/Pages/Home";
+import Review from "./components/Pages/Review";
+import About from "./components/Pages/About";
+
+
+
+function App() {
+
+  return (
+    <>
+      <Header text="Review Application" />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/review" element={<Review />} />
+        </Routes>
+      </Router>
+
+    </>
+  )
+}
+
+export default App
